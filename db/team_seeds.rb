21a16baf2aa558216e39d5ae4c015d1a69150c7f
amygoldberg <amykgoldberg@gmail.com>
2015-07-09 11:30:37 -0400

    initial commit

---
+++ db/team_seeds.rb
@@ -0,0 +1,42 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Team.destroy_all
+
+team = Team.create!(name: 'dallas cowboys', logo: '')
+team = Team.create!(name: 'new england patriots', logo: '')
+team = Team.create!(name: 'philadelphia eagles', logo: '')
+team = Team.create!(name: 'seattle seahawks', logo: '')
+team = Team.create!(name: 'oakland raiders', logo: '')
+team = Team.create!(name: 'san fran 49s', logo: '')
+team = Team.create!(name: 'green bay packers', logo: '')
+team = Team.create!(name: 'minnesota vikings', logo: '')
+team = Team.create!(name: 'pittsburgh steelers', logo: '')
+team = Team.create!(name: 'chicago bears', logo: '')
+team = Team.create!(name: 'denver broncos', logo: '')
+team = Team.create!(name: 'miami dolphins', logo: '')
+team = Team.create!(name: 'baltimore ravens', logo: '')
+team = Team.create!(name: 'tennessee titans', logo: '')
+team = Team.create!(name: 'new york jets', logo: '')
+team = Team.create!(name: 'washington redskins', logo: '')
+team = Team.create!(name: 'new york giants', logo: '')
+team = Team.create!(name: 'new orleans saints', logo: '')
+team = Team.create!(name: 'houston texans', logo: '')
+team = Team.create!(name: 'cleveland browns', logo: '')
+team = Team.create!(name: 'buffalo bills', logo: '')
+team = Team.create!(name: 'detroit lions', logo: '')
+team = Team.create!(name: 'san diego chargers', logo: '')
+team = Team.create!(name: 'carolina panthers', logo: '')
+team = Team.create!(name: 'indianpolis colts', logo: '')
+team = Team.create!(name: 'arizona cardinals', logo: '')
+team = Team.create!(name: 'atlanta falcons', logo: '')
+team = Team.create!(name: 'jacksonville jaguars', logo: '')
+team = Team.create!(name: 'tampa bay buccaneers', logo: '')
+team = Team.create!(name: 'kansas city chiefs', logo: '')
+team = Team.create!(name: 'st. louis rams', logo: '')
+team = Team.create!(name: 'cincinnati bengals', logo: '')
